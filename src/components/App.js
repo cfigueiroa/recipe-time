4b8ab1294a21f96a4415a9f2a69a530169ebd587
cfigueiroa <cfigueiroa@gmail.com>
2022-12-07 00:07:25 -0300

    start

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Receita from "./Receita";
+import Receitas from "./Receitas";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Receitas />} />
+        <Route path="/receita" element={<Receita />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
